0975849202fa98723e8a4c562145953c66921511
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-11-05 00:56:28 +0200

    add the user and post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true  
+  validates :password, presence: true
+  has_many :posts
 end
